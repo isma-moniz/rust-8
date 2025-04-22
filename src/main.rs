e13db9ea42f2abf97120b90748ba13efcd79a405
Ismael Moniz <hismamoniz@gmail.com>
2025-04-23 00:59:07 +0100

    added sdl2 stub (sort of)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
+
 const FONT: [u8; 80] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -26,6 +33,8 @@ pub struct Chip8 {
     sound_timer: u8,
     video_buffer: [u32; 64 * 32],
     registers: [u8; 16],
+    keypad: [bool; 16],
+    opcode: u16,
 }
 
 impl Chip8 {
@@ -46,10 +55,47 @@ impl Chip8 {
             sound_timer: 0,
             video_buffer: [0; 64 * 32],
             registers: [0; 16],
+            keypad: [false; 16],
+            opcode: 0,
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rust-8", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+    }
+
+    canvas.present();
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 }
